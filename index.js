be9d54f4907b30fb6e653b16a01bb5cd70ff0012
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-09-10 14:58:36 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
-let diceresult = document.getElementById("diceresult");
-let diceimage = document.getElementById("diceimage");
-
 function rolldice() {
-    
+
+    const numofdice = document.getElementById("numofdice").value;
+    const diceresult = document.getElementById("diceresult");
+    const diceimage = document.getElementById("diceimage");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numofdice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice-images/${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceresult.textContent = `dice: ${values.join(', ')}`;
+    diceimage.innerHTML = images.join('');
+
 }
